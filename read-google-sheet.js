866fc10b94e94a4666938764137c18549407fe65
sakumari <sakumari@zynga.com>
2024-05-03 18:20:35 +0530

    added read-google-sheet.js

---
+++ read-google-sheet.js
@@ -0,0 +1,36 @@
+const { google } = require('googleapis');
+const fs = require('fs');
+
+// Load the service account key JSON file.
+const serviceAccount = JSON.parse(process.env.SERVICE_ACCOUNT_KEY);
+
+const spreadsheetId = '19r8IWr7xwpP2NhtQzs_5m00FaKbGcOfN95uiyr55zH4';
+// Define the scopes for the Google Sheets API.
+const scopes = ['https://www.googleapis.com/auth/spreadsheets.readonly'];
+
+// Authenticate with the Google Sheets API.
+const auth = new google.auth.JWT(serviceAccount.client_email, null, serviceAccount.private_key, scopes);
+
+const sheets = google.sheets({ version: 'v4', auth });
+
+sheets.spreadsheets.values.get(
+	{
+		spreadsheetId: spreadsheetId,
+		range: 'Sheet1',
+	},
+	(err, res) => {
+		if (err) return console.log('The API returned an error: ' + err);
+		const rows = res.data.values;
+		if (rows.length) {
+			console.log('Data from Google Sheet:');
+			let data = '';
+			rows.map((row) => {
+				console.log(row.join(', '));
+				data += row.join(', ') + '\n';
+			});
+			fs.writeFileSync('./app-ads.txt', data);
+		} else {
+			console.log('No data found.');
+		}
+	}
+);
